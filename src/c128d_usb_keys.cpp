340e5d5b234972967d16d775e5167a3aee0f0556
Justin May <may.justin@gmail.com>
2021-08-25 08:48:42 -0700

    Init pins and send to output

--- src/c128d_usb_keys.cpp
+++ src/c128d_usb_keys.cpp
@@ -1,5 +1,19 @@
 #include <Arduino.h>
+#include "USBHost_t36.h"
 #include "c128d_usb_keys.hpp"
+
+#define C128_KEY_CAPSLOCK_TOGGLE USB_KEY_F4
+#define C128_KEY_4080_TOGGLE     USB_KEY_F7
+
+bool is_capslock_on;
+bool is_4080_on;
+
+
+void initialize_lock_key_state() {
+    // TODO: load state from EEPROM and set LEDs
+    is_capslock_on = false;
+    is_4080_on = false;
+}
 
 
 void initialize_usb_key_buffer() {
@@ -9,10 +23,18 @@ void initialize_usb_key_buffer() {
 }
 
 
-void initialize_pins_state() {
+void initialize_keyboard_output_pins() {
     for (int i=0; i < OUTPUT_PINS_COUNT; i++) {
         pins_state[i]->is_set = false;
+
+        // Set output to digital low to be safe
+        pinMode(pins_state[i]->pin_num, OUTPUT);
+        digitalWrite(pins_state[i]->pin_num, LOW);
+
+        // Set pin to input mode to make it appear disconnected to the C128d
+        pinMode(pins_state[i]->pin_num, INPUT);
     }
+
 }
 
 
@@ -33,5 +55,85 @@ void remove_keycode_from_buffer(uint8_t key_code) {
             return;
         }
     }
+}
+
+void _set_output_key(KeyInfo key_info) {
+    if (key_info.is_sent) {
+        key_info.col_pin->is_set = true;
+        key_info.row_pin->is_set = true;
+    }
+}
+
+
+void _toggle_caps_lock_state() {
+    // TODO: Set LED on/off and store to EEPROM
+    is_capslock_on = !is_capslock_on;
+    if (is_capslock_on) _set_output_key(usb_key_mapping[USB_KEY_F4]);
+}
+
+
+void _toggle_4080_lock_state() {
+    // TODO: Set LED on/off and store to EEPROM
+    is_4080_on = !is_4080_on;
+    if (is_4080_on) _set_output_key(usb_key_mapping[USB_KEY_F7]);
+}
+
+
+void _reset_keyboard_output_pins_state() {
+    for (int i=0; i < OUTPUT_PINS_COUNT; i++) {
+        pins_state[i]->is_set = false;
+    }
+}
+
+
+void _apply_keyboard_output_pins_state() {
+    for (int i=0; i < OUTPUT_PINS_COUNT; i++) {
+        if (pins_state[i]->is_set) {
+            pinMode(pins_state[i]->pin_num, OUTPUT);
+            digitalWrite(pins_state[i]->pin_num, LOW);
+        } 
+        else {
+            pinMode(pins_state[i]->pin_num, INPUT);
+        }
+    }
+}
+
+
+void update_output_pins(KeyboardController keyboard_controller) {
+    _reset_keyboard_output_pins_state();
+
+    // Set output pins state for every key in the buffer
+    for (int i=0; i < KEY_BUFFER_SIZE; i++) {
+        uint8_t usb_key_code = usb_key_buffer[i];
+
+        // If this is a pressed key, and is within the accepted range 
+        if ((usb_key_code > 0) && (usb_key_code <= MAX_USB_KEY_CODE)) {
+            // If caps lock is on, set left-shift 
+            if (keyboard_controller.capsLock()) {
+                _set_output_key(usb_key_mapping[USB_KEY_LSHIFT]);
+            }
+
+            // If up-arrow or left-arrow is pressed, set right-shift
+            if ((usb_key_code == USB_KEY_LEFT) || (usb_key_code == USB_KEY_UP)) {
+                _set_output_key(usb_key_mapping[USB_KEY_RSHIFT]);
+            }
+
+            // If caps-lock key (F4 on USB keyboard) is pressed toggle the C128 caps-lock key's state
+            if (usb_key_code == USB_KEY_F4) {
+                _toggle_caps_lock_state();
+            }
+            // If 40/80 key (F7 on USB keyboard) is pressed toggle the C128 key's 40/80 state
+            else if (usb_key_code == USB_KEY_F7) {
+                _toggle_4080_lock_state();
+            }
+            // This is not a lock key send it as is
+            else {
+                _set_output_key(usb_key_mapping[usb_key_code]);
+            }
+
+        }
+    }
+
+    _apply_keyboard_output_pins_state();
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,9 @@ void setup() {
 	Serial.begin(115200);
 
 	// Initialize data structures
-	initialize_pins_state();
 	initialize_usb_key_buffer();
+	initialize_lock_key_state();
+	initialize_keyboard_output_pins();
 
 	// Setup USB Host and listen to the first keyboard found
 	usb_host.begin();
@@ -42,10 +43,10 @@ void setup() {
 
 
 void loop() {
-	// Poll the USB keyboard and fire events for key-on or key-off events
+	// Poll the USB keyboard and update the USB key buffer
 	usb_host.Task();
 
 	// Once the USB keyboard buffer has been updated, derive output state from it
-
+	update_output_pins(keyboard);
 }
 
