ca77387f1393724f9d24742f7bbfccce22b9457d
Justin May <may.justin@gmail.com>
2021-10-24 16:57:08 -0700

    Fix test runner linker errors for Teensy

--- test/test_common/test_common.cpp
+++ test/test_common/test_common.cpp
@@ -4,11 +4,17 @@
 #ifdef ARDUINO
 #include <Arduino.h>
 
+// I found the solution to this issue at https://forum.pjrc.com/threads/29177-Teensy-3-1-signalr-c-(-text-_kill_r-0xe)-undefined-reference-to-_kill-error
+extern "C"{
+  int _write(){return -1;}
+}
+
+
 void setup() {
     // NOTE!!! Wait for >2 secs
     // if board doesn't support software reset via Serial.DTR/RTS
     // TODO: See if this actually needs to happen on Teensy
-    // delay(2000);
+    delay(2000);
     run_usb_keyboard_buffer_tests();
     run_lock_key_tests();
     run_usb_c128d_tests();
@@ -16,9 +22,10 @@ void setup() {
 
 void loop() {
     // Flash LED to indicate tests are done
-    digitalWrite(LED_BUILTIN, HIGH);
+	pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
     delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(13, LOW);
     delay(500);
 }
 
