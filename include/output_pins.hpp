cb74b7a12e30bb7e3d926258257345964005bf39
Justin May <may.justin@gmail.com>
2021-10-27 10:24:45 -0700

    Use const instead of #define

--- include/output_pins.hpp
+++ include/output_pins.hpp
@@ -2,33 +2,31 @@
 #define OUTPUT_PINS_HPP
 
 // Teensy 3.6 digital output pin numbers for the C128d keyboard matrix
-#define ROW0_PIN         22
-#define ROW1_PIN         21
-#define ROW2_PIN         20
-#define ROW3_PIN         15
-#define ROW4_PIN         18
-#define ROW5_PIN         17
-#define ROW6_PIN         16
-#define ROW7_PIN         19
-#define RESTORE_PIN      14
-#define FORTY_EIGHTY_PIN 11
-#define CAPS_LOCK_PIN    12
-#define COL0_PIN         23
-#define COL1_PIN         6
-#define COL2_PIN         5
-#define COL3_PIN         4
-#define COL4_PIN         3
-#define COL5_PIN         2
-#define COL6_PIN         1
-#define COL7_PIN         7
-#define K0_PIN           8
-#define K1_PIN           9
-#define K2_PIN           10
-#define RESTORE0_PIN     13
+const uint8_t  ROW0_PIN         = 22;
+const uint8_t  ROW1_PIN         = 21;
+const uint8_t  ROW2_PIN         = 20;
+const uint8_t  ROW3_PIN         = 15;
+const uint8_t  ROW4_PIN         = 18;
+const uint8_t  ROW5_PIN         = 17;
+const uint8_t  ROW6_PIN         = 16;
+const uint8_t  ROW7_PIN         = 19;
+const uint8_t  RESTORE_PIN      = 14;
+const uint8_t  FORTY_EIGHTY_PIN = 11;
+const uint8_t  CAPS_LOCK_PIN    = 12;
+const uint8_t  COL0_PIN         = 23;
+const uint8_t  COL1_PIN         = 6;
+const uint8_t  COL2_PIN         = 5;
+const uint8_t  COL3_PIN         = 4;
+const uint8_t  COL4_PIN         = 3;
+const uint8_t  COL5_PIN         = 2;
+const uint8_t  COL6_PIN         = 1;
+const uint8_t  COL7_PIN         = 7;
+const uint8_t  K0_PIN           = 8;
+const uint8_t  K1_PIN           = 9;
+const uint8_t  K2_PIN           = 10;
+const uint8_t  RESTORE0_PIN     = 13;
 
-
-#define FORTY_EIGHTY_LOCK_LED 29
-#define CAPSLOCK_LOCK_LED 30 
-
+const uint8_t FORTY_EIGHTY_LOCK_LED = 29;
+const uint8_t CAPSLOCK_LOCK_LED = 30;
 
 #endif
